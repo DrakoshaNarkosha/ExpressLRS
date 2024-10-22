721ad5a5665410e2484e209796cfe3bbeea98890
Dima <nsdimans@gmail.com>
2024-10-22 18:23:13 +0300

    SX127x driver improvement
    - Removed double call of ConfigLoraDefault().
    - Add PA_DAC register writing in case of PA_BOOST pin usage.

--- src/lib/SX127xDriver/SX127xRegs.h
+++ src/lib/SX127xDriver/SX127xRegs.h
@@ -133,7 +133,10 @@ typedef enum
 #define SX127X_OCP_ON 0b00100000    //  5     5     PA overload current protection enabled
 #define SX127X_OCP_TRIM 0b00001011  //  4     0     OCP current: I_max(OCP_TRIM = 0b1011) = 100 mA
 #define SX127X_OCP_150MA 0b00010010 //  4     0     OCP current: I_max(OCP_TRIM = 10010) = 150 mA
+#define SX127X_OCP_240MA 0b00011111 //  4     0     OCP current: I_max(OCP_TRIM = 11111) = 240 mA
 #define SX127X_OCP_MASK 0b00111111
+// SX127x_REG_PA_DAC
+#define SX127X_PA_DAC_HIGH_PWR 0b10000111
 
 // SX127X_REG_LNA
 #define SX127X_LNA_GAIN_0 0b00000000    //  7     5     LNA gain setting:   not used
